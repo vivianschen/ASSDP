7197c7ce9adc7b1934375f1316937df49e5984b5
schao0531 <staceychao2019@u.northwestern.edu>
2019-03-08 13:46:37 -0600

    implemented chorus detection by beat onset strength

--- test.py
+++ test.py
@@ -7,11 +7,15 @@ import librosa.display
 import soundfile as sf
 import matplotlib
 import matplotlib.pyplot as plt
-import pylab
+
+from pychorus import find_and_output_chorus
 from pychorus import create_chroma
 from pychorus.similarity_matrix import TimeTimeSimilarityMatrix, TimeLagSimilarityMatrix, Line
 import msaf
 import sys
+from dtw import dtw, accelerated_dtw
+from scipy.spatial.distance import cdist
+from math import isinf
 
 # Denoising size in seconds
 SMOOTHING_SIZE_SEC = 2.5
@@ -27,11 +31,6 @@ NUM_ITERATIONS = 20
 
 # We allow an error proportional to the length of the clip
 OVERLAP_PERCENT_MARGIN = 0.2
-
-#chorus_start_sec = find_and_output_chorus("chorus_detect.wav", "test.wav", 7)
-
-#chorus_find = find_chorus()
-
 
 def local_maxima_rows(denoised_time_lag):
     """Find rows whose normalized sum is a local maxima"""
@@ -109,59 +108,73 @@ def sorted_segments(line_scores):
     lines_to_sort.sort(key=lambda x: (x[1], x[2]), reverse=True)
     return lines_to_sort
 
+def fastdtw(x, y, dist, warp=1):
+    assert len(x)
+    assert len(y)
+    if np.ndim(x) == 1:
+        x = x.reshape(-1, 1)
+    if np.ndim(y) == 1:
+        y = y.reshape(-1, 1)
+    r, c = len(x), len(y)
+    D0 = np.zeros((r + 1, c + 1))
+    D0[0, 1:] = np.inf
+    D0[1:, 0] = np.inf
+    D1 = D0[1:, 1:]
+    D0[1:, 1:] = cdist(x, y, dist)
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            min_list = [D0[i, j]]
+            for k in range(1, warp + 1):
+                min_list += [D0[min(i + k, r), j],
+                             D0[i, min(j + k, c)]]
+            D1[i, j] += min(min_list)
+    return D1[-1, -1] / sum(D1.shape)
 
 
-file_name = "beethoven.wav"
+file_name = "Since_U_Been_Gone.wav"
+
 chroma, song_wav_data, sr, song_length_sec = create_chroma("audio/" + file_name)
+
+#chorus_start_sec = find_and_output_chorus("audio/foo.wav", "chorus.wav", 15)
 
 num_samples = chroma.shape[1]
 time_time_similarity = TimeTimeSimilarityMatrix(chroma, sr)
 time_lag_similarity = TimeLagSimilarityMatrix(chroma, sr)
 
 #time_time_similarity.display()
+print(msaf.get_all_label_algorithms())
+print(msaf.get_all_boundary_algorithms())
 
 #novelty based segmentation
 #uses the foote or checkerboard kernel method of segmenting songs
-sonified_file = "my_boundaries.wav"
-boundaries, labels = msaf.process("audio/" + file_name, boundaries_id="foote", plot=False,
-                                    sonify_bounds=True,labels_id="fmc2d",
-                                  out_bounds=sonified_file, out_sr=sr)
+#plot = True
+boundaries, labels = msaf.process("audio/" + file_name, feature="mfcc", boundaries_id="foote",
+                                    labels_id="fmc2d", out_sr=sr)
 
-print(boundaries)
-print(labels)
 #audio = librosa.load(sonified_file, sr=sr)[0]
 
+new_boundaries = []
+new_labels = []
+segment_nums = []
+mfccs = []
 idx = 0
 for x in range(len(boundaries) - 1):
-    if boundaries[x + 1] - boundaries[x] >= 1:
+    if boundaries[x + 1] - boundaries[x] >= 3:
         print("segment found at {0:g} min {1:.2f} sec".format(
                 boundaries[x] // 60, boundaries[x] % 60))
         segment_wav_data = song_wav_data[int(boundaries[x]*sr) : int(boundaries[x + 1]*sr)]
+
+        mel_freq = librosa.feature.mfcc(segment_wav_data, sr)
         sf.write("segment_{}.wav".format(idx), segment_wav_data, sr)
+
+        new_boundaries.append(boundaries[x])
+        new_labels.append(labels[x])
+        segment_nums.append(idx)
+        mfccs.append(np.average(mel_freq, axis=0))
     idx += 1
 
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-frames = open("labels/" + file_name + "_labels.txt", "w")
-for e in range(len(boundaries)-1):
-    outer_bound = e+1
-    frames.write(str(boundaries[e]) + "\t" + str(boundaries[outer_bound]) + "\t" + alphabet[int(labels[e])] + "\n")
-
-
-sys.exit(1)
-
-
-
-
-
-
-R = librosa.segment.recurrence_matrix(time_time_similarity.matrix, mode='affinity')
-plt.imshow(R)
-librosa.display.specshow(R, x_axis='time', y_axis='time', sr=time_time_similarity.sample_rate / (N_FFT / 2048))
-
-plt.show()
-
-time_time_similarity.display()
-time_lag_similarity.display()
+#pdb.set_trace()
 
 
 chroma_sr = num_samples / song_length_sec
@@ -181,7 +194,7 @@ lines = detect_lines(time_lag_similarity.matrix, candidate_rows,
 
 if len(lines) == 0:
     print("No choruses were detected. Try a smaller search duration")
-
+    sys.exit(-1)
 
 
 line_scores = count_overlapping_lines(
@@ -200,33 +213,87 @@ for c in choruses:
 unsorted_chorus_times.sort(key=lambda x: x[0])
 #print(unsorted_chorus_times)
 
-
-#merge overlapping intervals!!
+merge overlapping intervals!!
 chorus_times = []
 chorus_times.append(unsorted_chorus_times[0])
 for i in range(1, len(unsorted_chorus_times)):
     #pdb.set_trace()
     if (unsorted_chorus_times[i][0] - chorus_times[-1][0]) >= clip_length:
         chorus_times.append(unsorted_chorus_times[i])
-#
-# chorus_times = [list(i) for i in chorus_times]
-# merged_chorus_times = []
-# merged_chorus_times.append(chorus_times[0])
-# for curr in chorus_times:
-#     prev = merged_chorus_times[-1]
-#     if curr[0] <= prev[1]:
-#         #pdb.set_trace()
-#         prev[1] = max(prev[1], curr[1])
-#     else:
-#         merged_chorus_times.append(curr)
-#
-# print(merged_chorus_times)
 
+
+max_onset = 0
+best_chorus = []
 idx = 0
 for time in chorus_times:
-    print("chorus found at {0:g} min {1:.2f} sec".format(
-            time[0] // 60, time[0] % 60))
-    chorus_wave_data = song_wav_data[int(time[0]*sr) : int(time[1]*sr)]
-    sf.write("repeated_segment_{}.wav".format(idx), chorus_wave_data, sr)
-    idx += 1
-#time_lag_similarity.display()
+    if 10 <= (time[1] - time[0]) and (time[1] - time[0]) <= 30:
+        print("chorus found at {0:g} min {1:.2f} sec".format(
+                time[0] // 60, time[0] % 60))
+        chorus_wave_data = song_wav_data[int(time[0]*sr) : int(time[1]*sr)]
+        onset_detect = librosa.onset.onset_detect(chorus_wave_data, sr)
+        print(np.mean(onset_detect))
+        if np.mean(onset_detect) >= max_onset:
+            max_onset = np.mean(onset_detect)
+            best_chorus = chorus_wave_data
+        sf.write("chorus_segment_{}.wav".format(idx), chorus_wave_data, sr)
+        idx += 1
+
+sf.write("best_chorus_segment.wav", best_chorus, sr)
+chorus_mfcc = np.average(librosa.feature.mfcc(best_chorus, sr), axis=0)
+
+
+
+
+#onset, frequency bands filled up, peakiness, more frequency bands of roughly
+#equal energy, beat strength,
+
+structure_labels = [""] * len(new_labels)
+
+euclidean_norm = lambda x, y: np.abs(x - y)
+for x in range(len(new_boundaries)):
+    chorus_sect = -1
+    dist = fastdtw(mfccs[x], chorus_mfcc, dist=euclidean_norm)
+    #TODO: normalize threshold
+    if dist < 2 or new_labels[x] == chorus_sect:
+        chorus_sect = new_labels[x]
+        structure_labels[x] = "chorus"
+    print(dist)
+    print()
+
+for x in range(len(new_boundaries)):
+    found_match = False
+    for y in range(x + 1, len(new_boundaries)):
+        if (new_labels[x] == new_labels[y]) and structure_labels[y] == "":
+            found_match = True
+            structure_labels[x] = "verse"
+            structure_labels[y] = "verse"
+    if found_match == False and structure_labels[x] == "":
+        if x == 0:
+            structure_labels[x] = "intro"
+        elif x == (len(new_boundaries) - 1):
+            structure_labels[x] = "outro"
+        else:
+            structure_labels[x] = "transition"
+
+
+# for x in range(len(new_boundaries)):
+#     for y in range(x + 1, len(new_boundaries)):
+#         dist = fastdtw(mfccs[x], mfccs[y], dist=euclidean_norm)
+#         print(x + 1)
+#         print(y + 1)
+#         print(dist)
+#         print()
+
+print(new_boundaries)
+print(new_labels)
+print(structure_labels)
+
+# alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+# frames = open("labels/" + file_name + "_labels.txt", "w")
+# for e in range(len(new_boundaries)):
+#     if e < len(new_boundaries) - 1:
+#         outer_bound = e+1
+#         frames.write(str(new_boundaries[e]) + "\t" + str(new_boundaries[outer_bound]) + "\t" + alphabet[int(new_labels[e])] + "\n")
+#     else:
+#         frames.write(str(new_boundaries[e]) + "\t" + str(song_length_sec) + "\t" + alphabet[int(new_labels[e])] + "\n")
+#
